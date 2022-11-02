07c8a99546393d7c46737e49a1e46af382b79dd6
MARO <mathieu.tammaro@octo.com>
2022-11-02 16:34:40 +0100

    [WIP] testing prices of differents books

--- __tests__/sellBooks.test.ts
+++ __tests__/sellBooks.test.ts
@@ -2,13 +2,42 @@ import sellBooks from '../src/sellBooks'
 
 describe('sellBooks', () => {
   describe('computePrice', () => {
-    test('Compute the price of one book.', () => {
+    test('Compute the price of one book and expect it to be 8', () => {
       //Given
       var books = ['Tome 1']
+      var expectedPrice = 8
       //When
       var computedPrice = sellBooks.computePrice(books)
       //Then
-      expect(computedPrice).toBe(sellBooks.bookPrice)
+      expect(computedPrice).toBe(expectedPrice)
+    })
+  })
+})
+
+describe('sellBooks', () => {
+  describe('computePrice', () => {
+    test('Compute the price of two differents books and expect it to be 15.2 (5% reduction)', () => {
+      //Given
+      var books = ['Tome 1', 'Tome 2']
+      var expectedPrice = 2 * 8 * 0.95
+      //When
+      var computedPrice = sellBooks.computePrice(books)
+      //Then
+      expect(computedPrice).toBe(expectedPrice)
+    })
+  })
+})
+
+describe('sellBooks', () => {
+  describe('computePrice', () => {
+    test('Compute the price of three differents books and expect it to be 21.6 (10% reduction)', () => {
+      //Given
+      var books = ['Tome 1', 'Tome 2', 'Tome 3']
+      var expectedPrice = 3 * 8 * 0.9
+      //When
+      var computedPrice = sellBooks.computePrice(books)
+      //Then
+      expect(computedPrice).toBe(expectedPrice)
     })
   })
 })
--- src/sellBooks.ts
+++ src/sellBooks.ts
@@ -1,7 +1,16 @@
-const bookPrice = 8
-const computePrice = (books: string[]): number => 8
+const computePrice = (books: string[]): number => {
+  switch (books.length) {
+    case 1:
+      return 8
+    case 2:
+      return 15.2
+    case 3:
+      return 21.6
+    default:
+      return 0
+  }
+}
 
 export default {
-  bookPrice,
   computePrice,
 }
